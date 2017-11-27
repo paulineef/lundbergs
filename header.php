891dd23e25600af8069ebd6a5e22ebe615d38273
Frida <Frida@wlanguest86108.eduroam.hj.se>
2017-11-27 15:36:50 +0100

    hej

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device=width, initial-scale=1.0">
+			<link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>"
+			<title><?php bloginfo('name')?></title>
+			<?php wp_head(); ?>
+		</head>
+		<body>
+			<?php get_template_part('partials/sitehead'); ?>
---
+++ partials/sitehead.php
@@ -0,0 +1,3 @@
+<h1>hej klara :)</h1>
+
+<php wp_nav_menu(); ?>
