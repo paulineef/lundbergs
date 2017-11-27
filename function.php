46c202255b8ac60142b9b14c4f140f420de737dc
Frida <Frida@wlanguest86108.eduroam.hj.se>
2017-11-27 15:57:45 +0100

    menu

--- function.php
+++ function.php
@@ -1,18 +1,7 @@
-<?php 
-
-function register_the_menus() {
-	register_nav_menus(
-	array(
-		'header-menu' => __('Header Menu'),
-		'extra-menu' => __('Extra Menu')
-		)
-	);
-} 
-
-add_action('init', 'register_my_menus');
+<?php  
+	include 'partials/reg_menu.php';
 
 ?>
-
 
 
  <!-- <?php  
