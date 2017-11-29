a80d8bd0d229766d64b7ec86394262d12d4bb932
lisaarman <lisaarman2@gmail.com>
2017-11-29 09:56:16 +0100

    a

--- partials/sitehead.php
+++ partials/sitehead.php
@@ -1,4 +1,6 @@
-<div id="headerMenu" class="menu"><?php wp_nav_menu(); ?></div>
+<header>
+	<div id="headerMenu" class="menu"><?php wp_nav_menu(); ?></div>
+	</header>
 <h1>hej klara :)</h1>
 <style>
 </style>
