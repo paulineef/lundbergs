69c09c14729263a6213abce2f9d031e1c68bb744
lisaarman <lisaarman2@gmail.com>
2017-11-30 12:22:16 +0100

    w

--- partials/sitehead.php
+++ partials/sitehead.php
@@ -1,6 +1,12 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <header>
 	<div id="headerMenu" class="menu"><?php wp_nav_menu(); ?></div>
 	<i id="menu" class="fa fa-bars" aria-hidden="true"></i>
 	</header>
-<style>
-</style>
+<script>
+$(document).ready(function() {					// makes sure code does not execute until entire DOM is loaded
+			$("#menu").click(function(){
+			$("#headerMenu").toggleClass('open');
+			});
+}
+</script>
