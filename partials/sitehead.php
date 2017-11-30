18ae00adf25435017e1b47ff6903406146d26f09
lisaarman <lisaarman2@gmail.com>
2017-11-30 12:42:53 +0100

    k

--- partials/sitehead.php
+++ partials/sitehead.php
@@ -1,4 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700" rel="stylesheet">
 <header>
 	<div id="headerMenu" class="menu"><?php wp_nav_menu(); ?></div>
 	<i id="menu" class="fa fa-bars" aria-hidden="true"></i>
