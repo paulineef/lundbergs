2bc20f9ab66bf9840430b3d611a266abb2d7a05b
lisaarman <lisaarman2@gmail.com>
2017-12-01 10:07:35 +0100

    e

--- front-page.php
+++ front-page.php
@@ -3,6 +3,10 @@
 	<h2>
 		Svensktillverkad </br> avfallshantering </br> sedan 1896
 	</h2>
-	<div id="arrow"><a href="#"></a></div>
+	<div id="arrow">
+		<a href="#">
+			<div id="arrowImg"></div>
+		</a>
+	</div>
 </div>
 <?php get_footer(); ?>
