10c765240e417f0bde880c7ac92962ed8ecea5ef
Pauline <960707@live.se>
2017-12-01 13:28:05 +0100

    infoo

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
 	</body>
 		<footer>
+			<div id="info">
+				Tel 010-102 25 50<br/><br/>  
+				Klingekärrsgatan 10<br/> 
+				566 31 Habo<br/> 
+				Sverige
+			</div>
 			<div id="footerLogo">
-				
+			</div>
+			<div id="info">
 			</div>
 		</footer>
 	<?php wp_footer(); ?>
