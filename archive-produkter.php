f45f78b106bc8138871985ac11af2e8e77150949
lisaarman <lisaarman2@gmail.com>
2017-12-04 16:23:50 +0100

    e

--- archive-produkter.php
+++ archive-produkter.php
@@ -6,26 +6,44 @@
 			the_post(); 
 ?>
 
-<ul>
-  <li>Coffee</li>
-  <li>Tea</li>
-  <li>Milk</li>
-</ul>
-
-
-
 <ul class="staffGrid">
-	<a href='<?php the_permalink();?>'><li><h1 class="titlePosts">
+	<li class="product"><a href='<?php the_permalink();?>'><h1 class="titlePosts">
 
-	<?php the_title();//the_ = echo out ?></h1></li></a>
+		<h2><?php the_title();?></h2> <?php the_post_thumbnail(); ?></h1></a></li>
+</ul>
 
 <?php 
 	endwhile;
 	endif;
 ?>
 
-<?php // the_excerpt();
-	the_post_thumbnail();//echo out the content from the wp developer 
-?>
-
 <?php get_footer(); ?>
+<style>
+	.product {
+		max-width: 50%;
+		box-sizing: border-box;
+		float: left;
+		overflow: hidden;
+		padding: 88px;
+		position: relative;
+
+	}
+	.product img {
+		width: 100%;
+		height: 100%;
+	}
+	.product h2 {
+		color: black;
+		margin: 0;
+		text-align: center;
+		position: absolute;
+		margin: 0 auto;
+		top: 350px;
+	}
+	.product a {
+		text-decoration: none;
+	}
+	.product li {
+		list-style-type: none;
+	}
+</style>
