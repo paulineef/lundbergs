d245716ce5ab498ea289e44bd3a6c0550a52f0be
lisaarman <lisaarman2@gmail.com>
2017-12-04 14:02:21 +0100

    r

--- partials/sitehead.php
+++ partials/sitehead.php
@@ -8,8 +8,6 @@
 			<?php wp_nav_menu(); ?>
 		</div>
 		<div class="toplogo"></div>
-		
-		<div class="hamburger">&#9776;</div>
 		<div id="nav-icon3">
   				<span></span>
  				<span></span>
@@ -20,25 +18,8 @@
 	</nav>
 </header>
 <script>
-	$(document).ready(function() {
-		$( ".cross" ).hide();
-
-		$( ".hamburger" ).click(function() {
-			$( ".hide" ).slideToggle( "normal", function() {
-				$( ".hamburger" ).hide();
-				$( ".cross" ).show();
-			});
-		});
-
-		$( ".cross" ).click(function() {
-			$( ".hide" ).slideToggle( "normal", function() {
-				$( ".cross" ).hide();
-				$( ".hamburger" ).show();
-			});
-		});
-	});
-	
-	$("#nav-icon3").click(function(){
+	$(document).ready(function() {	
+		$("#nav-icon3").click(function(){
 			$("#nav-icon3").toggleClass('open');
 			$(".hide").toggleClass('open');
 			});
@@ -52,77 +33,5 @@
 	}
 	
 });
+		});
 </script>
-<style>
-	#menu.small #nav-icon3 {
-		top: 25px;
-		width: 55px;
-		height: 40px;
-		transition: 0.5s all ease 0s;
-	}
-	#nav-icon3{
-	  width: 65px;
-	  height: 50px;
-	  position: absolute;
-	  right: 32px;
-	  top: 35px;
-	  -webkit-transform: rotate(0deg);
-	  -moz-transform: rotate(0deg);
-	  -o-transform: rotate(0deg);
-	  transform: rotate(0deg);
-	  -webkit-transition: .5s ease-in-out;
-	  -moz-transition: .5s ease-in-out;
-	  -o-transition: .5s ease-in-out;
-	  transition: .5s ease-in-out;
-	  cursor: pointer;
-	}
-	#nav-icon3 span{
-	  display: block;
-	  position: absolute;
-	  height: 6px;
-	  width: 100%;
-	  background: #ffffff;
-	  opacity: 1;
-	  left: 0;
-	  -webkit-transform: rotate(0deg);
-	  -moz-transform: rotate(0deg);
-	  -o-transform: rotate(0deg);
-	  transform: rotate(0deg);
-	  -webkit-transition: .25s ease-in-out;
-	  -moz-transition: .25s ease-in-out;
-	  -o-transition: .25s ease-in-out;
-	  transition: .25s ease-in-out;
-	}
-
-	#nav-icon3 span:nth-child(1) {
-	  top: 0px;
-	}
-	#nav-icon3 span:nth-child(2),#nav-icon3 span:nth-child(3) {
-	  top: 20px;
-	}
-	#nav-icon3 span:nth-child(4) {
-	  top: 40px;
-	}
-	#nav-icon3.open span:nth-child(1) {
-	  top: 18px;
-	  width: 0%;
-	  left: 50%;
-	}
-	#nav-icon3.open span:nth-child(2) {
-	  -webkit-transform: rotate(45deg);
-	  -moz-transform: rotate(45deg);
-	  -o-transform: rotate(45deg);
-	  transform: rotate(45deg);
-	}
-	#nav-icon3.open span:nth-child(3) {
-	  -webkit-transform: rotate(-45deg);
-	  -moz-transform: rotate(-45deg);
-	  -o-transform: rotate(-45deg);
-	  transform: rotate(-45deg);
-	}
-	#nav-icon3.open span:nth-child(4) {
-	  top: 18px;
-	  width: 0%;
-	  left: 50%;
-	}
-</style>
