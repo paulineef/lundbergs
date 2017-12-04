23d540cea725518cd4ca22d8e40869e3964422dc
Pauline <960707@live.se>
2017-12-04 14:55:20 +0100

    yuo

--- archive-nyheter.php
+++ archive-nyheter.php
@@ -5,12 +5,16 @@
 		while ( have_posts()): 
 			the_post(); 
 ?>
+<ul>
+	<li>
+		<div id="nyheter">
+		<a href='<?php the_permalink()?>'>
+			<h1><?php the_title();//the_ = echo out ?></h1></a>
+				<p><?php the_content();?></p>
 
-<div id="nyheter">
-	<a href='<?php the_permalink()?>'>
-		<h1><?php the_title();//the_ = echo out ?></h1></a>
-			<p><?php the_content();?></p>
-</div>
+		</div>
+	</li>
+</ul>
 
 <?php 
 	endwhile;
