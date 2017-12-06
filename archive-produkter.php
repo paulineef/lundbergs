bfc8dfb93705d050cf0aae74c1402194f8cb1e29
lisaarman <lisaarman2@gmail.com>
2017-12-06 14:43:53 +0100

    n

--- archive-produkter.php
+++ archive-produkter.php
@@ -1,11 +1,7 @@
 <?php get_header(); ?>
 <div id="bigCont">
 	<div id="omOss">
-	<div id="omOssImg"></div> 
-	<div id="omOssContent"> 
-		<h1 class="bigHeading">Om oss</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet, mi eget blandit pellentesque, tortor risus molestie nibh, vitae aliquam dolor odio eu magna. Ut suscipit luctus sapien at lobortis. Morbi dignissim sem id commodo dapibus. Pellentesque ut mauris quis nisl sodales fringilla vel sit amet risus. Cras tristique sollicitudin sapien, at ullamcorper lectus aliquam non. In lacinia, neque at congue condimentum, ante diam fermentum quam, vitae iaculis sem ex et nulla.</p>
-	</div>
+	<div id="ProdukterImg"></div> 
 	
 </div>
 	<?php 
@@ -25,12 +21,21 @@
 				<span class="types">
 					<h3><a href="/kategori/<?php echo $value->slug ?>"><?php echo $value->name ?></a></h3>
 				</span>
+				
 			</div>
 			<?php } ?>
+	<div id="omOssContent"> 
+		<p id=pProdukt>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet, mi eget blandit pellentesque, tortor risus molestie nibh, vitae aliquam dolor odio eu magna. Ut suscipit luctus sapien at lobortis. Morbi dignissim sem id commodo dapibus. Pellentesque ut mauris quis nisl sodales fringilla vel sit amet risus. Cras tristique sollicitudin sapien, at ullamcorper lectus aliquam non. In lacinia, neque at congue condimentum, ante diam fermentum quam, vitae iaculis sem ex et nulla.</p>
+	</div>
+	
 </ul></div>
 </div>
 <?php get_footer(); ?>
 <style>
+	#pProdukt {
+		margin-top: 42px;
+		font-size: 12pt;
+	}
 	#bigCont {
 		min-height: 90vh;
 		overflow: hidden;
@@ -38,27 +43,27 @@
 	#catCon {
 		overflow: hidden;
 		text-align: center;
-		width: 600px;
+		width: 80%;
 		margin: 0 auto;
-		position: absolute;
-		top: 30vh;
+		margin-top: -150px;
 		left: 33%;
 	}
 	.cont {
-		width: 250px;
-		height: 200px;
+		width: 100%;
+		height: 300px;
 		display: inline;
 		overflow: hidden;
 		float: left;
-		margin-top: 18vh;
+		margin-top: 20vh;
 		text-align: center;
 		background: #6FA67F;
 		color: white;
-		margin: 22px;
+		margin: 22px 0;
 		box-sizing: border-box;
 	}
 	.cont h3 {
-		margin: 70px 10px;
+		margin: 120px 0px;
+		font-size: 25pt;
 	}
 	.cont a {
 		color: white;
@@ -101,10 +106,23 @@
 	.product li {
 		list-style-type: none;
 	}
-	@media (min-width: 1024px){
-		.product {
-			max-width: 50%;
-			padding: 88px;	
-		}
+@media (min-width: 1024px){
+	.product {
+		max-width: 50%;
+		padding: 88px;	
 	}
+	#pProdukt {
+		margin: 0 auto;
+	}
+	#omOssContent {
+		width: 100%;
+	}
+	.cont {
+		width: 50%;
+		margin: 10px;
+	}
+	#catCon {
+		width: 600px;
+	}
+}
 </style>
