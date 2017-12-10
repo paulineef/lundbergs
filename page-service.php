9bc3e159ce31ee58a34cd169159d21a943f269c2
klaranormaan <klaranormaan@users.noreply.github.com>
2017-12-10 20:08:09 +0100

    a

--- page-service.php
+++ page-service.php
@@ -4,8 +4,8 @@
 		<div id="omOss">
 			<div id="serviceImg"></div> 
 		</div>
-		<h1 id="serviceHead"> <?php the_title();?> </h1>
 		<div id="serviceContainer">
+			<h1 id="serviceHead"> <?php the_title();?> </h1>
 			<?php if(have_posts()):
 					while(have_posts()):
 						the_post();
@@ -13,8 +13,11 @@
 						<div id="serviceContentContainer">
 							<?php the_content(); //echo out the content of the about us page?>
 						</div>
-						<h1 id="faqHead"> FAQ </h1>
-						<div class="faqContainer"> <?php get_template_part('partials/posttype_faq'); //get the content from file faq ?></div>
+						
+						<div class="faqContainer"> 
+							<h1 id="faqHead"> FAQ </h1>
+							<?php get_template_part('partials/posttype_faq'); //get the content from file faq ?>
+						</div>
 						<?php
 					endwhile; 
 				endif;
