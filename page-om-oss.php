cd206a6a200a37ee233cf3f5515ff2e27f779f91
Pauline <960707@live.se>
2017-12-12 18:49:14 +0100

    om oss

--- page-om-oss.php
+++ page-om-oss.php
@@ -1,26 +1,46 @@
-<div id="omOss">
-	<div id="omOssImg"></div> 
-	<div id="omOssContent"> 
-		<h6 id="headomoss"><?php the_title(); ?></h6>
+<main>
+	<?php get_header();?>
+
+	<div id="omOss">
+		<?php
+			if(has_post_thumbnail()): 
+				$thumb_id = get_post_thumbnail_id();
+				$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+				$thumb_url = $thumb_url_array[0]; 
+		?>
+		<div id="omOssImg" style="background-image: url('<?php echo $thumb_url; ?>');"></div> 
+		<?php endif; ?>
+
+		<div id="omOssContent">
+			<?php 
+				the_post();?>
+				<p><?php the_content(); ?></p>
+		</div>
 	</div>
-</div>
-<?php get_header();?>
-	<?php 
-		if (have_posts()):
-			while (have_posts()): 
-				the_post(); 
-	?>
-				<div id="contentfloat"><?php  the_content();?></div>
-				
-				<ul id="staffUl">
-				<h1 class="bigHeading">Våra anställda</h1>
+	<div id="personal">
+		<h1>Personal</h1>
+		<ul>
+			
+			<?php 
+				$personal = new WP_Query(array(
+		                    'post_type' => 'personal',
+		                    'order' => 'ASC'
+		                	));
 
-				<?php get_template_part('partials/posttype_personal'); //get the content from the file personal?>
-	<?php 
-			endwhile;
-		endif;
-	?>
+				if ($personal->have_posts()):
+					while ($personal->have_posts()): 
+						$personal->the_post(); 
+			?>
+			<li>
+				<div class="anstalld" style="background-image: url('<?php the_post_thumbnail_url('mobil_thumbnail');?>');"></div>
+				<h4><?php the_title(); ?></h4>
+			</li>
+			<?php 
+				endwhile;
+				endif;
+			?>
+		</ul>
+	</div>
 
-</ul>
-<?php get_footer(); ?>
-
+	<?php get_footer(); ?>
+</main>
