cc56be87617e4ace87c4f3d1f799157d8465ff2b
lisaarman <lisaarman2@gmail.com>
2017-12-12 15:05:14 +0100

    e

--- incl/functions.php
+++ incl/functions.php
@@ -2,4 +2,19 @@
 
 add_theme_support('post-thumbnails'); 
 
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Home right sidebar',
+		'id'            => 'home_right_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+
+
 ?>
