5c5fe7a97993ec3b8331949cafe2c8c20146ed84
lisaarman <lisaarman2@gmail.com>
2017-12-14 20:54:34 +0100

    4

--- footer.php
+++ footer.php
@@ -1,17 +1,19 @@
 	</body>
 		<footer>
-			<div id="info">
-				Tel 010-102 25 50<br/><br/>  
-				Klingekärrsgatan 10<br/> 
-				566 31 Habo<br/> 
-				Sverige
-			</div>
-			<div id="footerLogo">
-			</div>
-			<div id="linkedin">
-				<a href="https://www.linkedin.com/company/18000722/" rel="external" target="_blank">
-					<i class="fa fa-linkedin-square" aria-hidden="true"></i>
-				</a>
+			<div id="footerCont">
+				<div id="info">
+					Tel 010-102 25 50<br/><br/>  
+					Klingekärrsgatan 10<br/> 
+					566 31 Habo<br/> 
+					Sverige
+				</div>
+				<div id="footerLogo">
+				</div>
+				<div id="linkedin">
+					<a href="https://www.linkedin.com/company/18000722/" rel="external" target="_blank">
+						<i class="fa fa-linkedin-square" aria-hidden="true"></i>
+					</a>
+				</div>
 			</div>
 		</footer>
 
