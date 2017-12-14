7e38ad85ff7fc1031361989c037c1bdf19e82f15
klaranormaan <klaranormaan@users.noreply.github.com>
2017-12-14 12:46:05 +0100

    j

--- functions.php
+++ functions.php
@@ -1,18 +1,3 @@
- <?php  
-
-// //function to link portfolio.css
-// //name, location, deps (have none for now),version, what device will it displays
-// function lundbergs_style_enqueue(){ 
-
-// 	wp_enqueue_style( 'css', get_stylesheet_uri() . '/css.css', false, '1.1', 'all' ); 
-// }
-
-// //to call with an action our function, to tell when to execute our function
-// //generate all the scripts that has to be included in our theme, what kind of function you want to call
-// //where should the script be embedded/print, is clarified in header.php
-// add_action( 'wp_enqueue_style', 'lundbergs_script_enqueue');
-
-?>
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 <?php 
 	//title bar
@@ -40,9 +25,4 @@
 	}
 	//Hooks a function on a specific action
 	add_action( 'init', 'register_my_menus' );
-
-add_action( 'init', 'sk_add_category_taxonomy_to_produkter' );
-function sk_add_category_taxonomy_to_produkter() {
-	register_taxonomy_for_object_type( 'kategori', 'e4gf_produkter' );
-}
 ?>
