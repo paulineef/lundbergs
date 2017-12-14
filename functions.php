8b7da9ff0364b475f802f264033a562aa7fe619a
lisaarman <lisaarman2@gmail.com>
2017-12-14 11:28:26 +0100

    d

--- functions.php
+++ functions.php
@@ -24,24 +24,20 @@
 	include('incl/post_types.php');
 	include('incl/taxonomies.php');
 
-
 	//Media sizes
 	add_image_size( 'grid_thumbnail', 300, 300, true );
 	add_image_size( 'mobile_grid', 500, 500, true );
 	add_image_size( 'single_large', 660, 400, false );
-
 
 	//register menus through a function
 	function register_my_menus() {
 	  register_nav_menus(
 		array(
 		  'main-menu' => __( 'Main Menu' ), //menu location and name of menu
-		  'extra-menu' => __( 'Extra Menu' ) //menu location and name of menu
 		)
 	  );
 	}
 	add_action( 'init', 'register_my_menus' );
-
 
 add_action( 'init', 'sk_add_category_taxonomy_to_produkter' );
 function sk_add_category_taxonomy_to_produkter() {
